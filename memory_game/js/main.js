520b164071e3378fac21efb16abde36bf3fdb913
Mika Mccarron <MikaMccarron@MacBook-Air-3.local>
2018-08-27 15:18:37 +1000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("UP and running!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardThree);
