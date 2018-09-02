46c55de1a53df766157ba3a1125186d8321e275d
Mika Mccarron <MikaMccarron@MacBook-Air-3.local>
2018-09-02 16:42:22 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,25 @@
 console.log("UP and running!")
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king";*/
 console.log("User flipped" + cardOne);
 console.log("User flipped" + cardThree);
+
+
+var cards = ["queen","queen","king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cards[0]);
+console.log("User flipped queen");
+
+
+var cardTwo = cards[2];
+cardsInPlay.push(cards[2]);
+console.log ("User flipped king");
+
+
+
+
+
+
